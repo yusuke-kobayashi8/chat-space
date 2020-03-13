20138dc5d36373839df00bf0416c2852b40d3003
yusuke-kobayashi8 <61771851+yusuke-kobayashi8@users.noreply.github.com>
2020-03-13 16:35:56 +0900

    新規グループ画面実装

---
+++ app/models/group.rb
@@ -0,0 +1,9 @@
+class Group < ApplicationRecord
+  # # Include default devise modules. Others available are:
+  # # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+  has_many :group_users
+  has_many :users, through: :group_users
+  validates :name, presence: true, uniqueness: true
+end
--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,11 @@ Bundler.require(*Rails.groups)
 module ChatSpace
   class Application < Rails::Application
     config.generators do |g|
-      g.stylesheets false
-      g.javascripts false
-      g.helper false
-      g.test_framework false
+    g.stylesheets false
+    g.javascripts false
+    g.helper false
+    g.test_framework false
+    config.i18n.default_locale = :ja
     end
   end
 end

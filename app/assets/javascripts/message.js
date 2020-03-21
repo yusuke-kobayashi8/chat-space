6897f7d9cb36639c82ec3303000e9947b23456c9
yusuke-kobayashi8 <61771851+yusuke-kobayashi8@users.noreply.github.com>
2020-03-21 17:25:10 +0900

    自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){
-  function buildHTML(message){
-    if ( message.image ) {
+  
+  var buildHTML = function(message) {
+    if ( message.content & message.image ) {
       var html =
-       `<div class="message_content_list">
+       `<div class="message_content_list" data-message-id = message.id >
           <div class="message_contents_list_text_1">
             <div class="message_contents_list_text_1_name">
               ${message.user_name}
@@ -18,10 +19,9 @@ $(function(){
           </div>
           <img src=${message.image} >
         </div>`
-      return html;
-    } else {
+    } else if (message.content) {
       var html =
-       `<div class="message_content_list">
+      `<div class="message_content_list" data-message-id = message.id >
           <div class="message_contents_list_text_1">
             <div class="message_contents_list_text_1_name">
               ${message.user_name}
@@ -36,9 +36,49 @@ $(function(){
             </p>
           </div>
         </div>`
-      return html;
+    } else if (message.image) {
+      var html =
+      `<div class="message_content_list" data-message-id = message.id >
+          <div class="message_contents_list_text_1">
+            <div class="message_contents_list_text_1_name">
+              ${message.user_name}
+            </div>
+            <div class="message_contents_list_text_1_day">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message_contents_list_text_2">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
     };
-  }
+    return html;
+  };
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages_contents').append(insertHTML);
+        $('.messages_contents').animate({ scrollTop: $('.messages_contents')});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -61,5 +101,8 @@ $(function(){
       alert('メッセージを送信できません');
     });
     return false;
-})
+  })
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
